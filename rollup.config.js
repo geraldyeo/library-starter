cbdeac99a73c7b743f4f835eba10c72a5d383cad
Gerald Yeo <gerald.yeo@gmail.com>
2016-02-02 12:26:17 +0800

    rollup config; refactor

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,8 @@
+import babel from 'rollup-plugin-babel';
+
 export default {
+	dest: 'lib/index.js',
 	entry: 'src/index.js',
 	format: 'cjs',
-	dest: 'lib/index.js'
+	plugins: [babel()]
 };
--- src/math.js
+++ src/math.js
@@ -1,8 +1,8 @@
-export function square ( x ) {
+export function square (x) {
 	return x * x;
 }
 
-export function cube ( x ) {
+export function cube (x) {
 	return x * x * x;
 }
 
