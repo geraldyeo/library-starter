29f6a32245aeb0e1823ee9a8722759086493499d
Gerald Yeo <gerald.yeo@gmail.com>
2016-02-03 16:06:14 +0800

    using webpack 2

--- rollup.config.js
+++
@@ -1,9 +0,0 @@
-import babel from 'rollup-plugin-babel';
-
-export default {
-	dest: 'lib/index.js',
-	entry: 'src/index.js',
-	format: 'cjs',
-	sourceMap: process.env.BABEL_ENV === 'production' ? true : 'inline',
-	plugins: [babel()]
-};
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var webpack = require('webpack');
+var yargs = require('yargs');
+
+var optimizeMinimize = yargs.alias('p', 'optimize-minimize').argv.optimizeMinimize;
+var dir_src = path.resolve(__dirname, 'src');
+var dir_lib = path.resolve(__dirname, 'lib');
+
+module.exports = {
+	devtool: optimizeMinimize ? 'source-map' : null,
+	entry: {
+		main: ['babel-polyfill', path.resolve(dir_src, 'index.js')]
+	},
+	output: {
+		path: dir_lib,
+		filename: 'index.js'
+	},
+	module: {
+		preLoaders: [
+			{ test: /\.js$/, loader: 'eslint-loader', exclude: /node_modules/ }
+		],
+		loaders: [
+			{ test: /\.js$/, loader: 'babel', exclude: /node_modules/ }
+		]
+	},
+	plugins: [
+		new webpack.NoErrorsPlugin()
+	],
+	eslint: {
+		configFile: path.resolve(__dirname, '.eslintrc')
+	}
+};
